39f4bef9db21e27ac83323783736e50015d56345
Manuel Aristarán <jazzido@jazzido.com>
2010-01-18 18:59:34 -0300

    ooh

--- lib/rmeaningtool.rb
+++ lib/rmeaningtool.rb
@@ -15,9 +15,16 @@ module RMeaningTool
   class Client
 
     def initialize(api_key, tree_key, base_url=MT_BASE_URL)
+      @api_key = api_key
+      @tree_key = tree_key
+      @base_url = base_url
     end
     
     def get_categories(source, input, options = {})
+      url = "#{@base_url}/categories"
+      
+      
+
     end
 
     def get_tags(source, input, options = {})
